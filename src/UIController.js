fadb5d1feb5a713c5e09fe50867d1592fd304881
jpmunich <parkercoyle8@gmail.com>
2023-11-30 11:32:40 -0500

    New function creates forecast display piece

--- src/UIController.js
+++ src/UIController.js
@@ -39,12 +39,29 @@ const createWeatherCell = (parent) => {
   icon.src = "../dist/images/thermometer.svg";
 
   const infoType = createTextElement("h6", textContainer, "Feels Like");
-  const info = createTextElement("h3", textContainer, "17°F");
+  const info = createTextElement("h3", textContainer, "17 °F");
 };
 
+const createForecastCell = (parent) => {
+  const container = createElementWithClass(
+    "div",
+    parent,
+    "forecast-cell-container"
+  );
+  const day = createTextElement("h4", container, "Monday");
+  const temperatureHigh = createTextElement("h2", container, "43 °F");
+  const temperatureLow = createTextElement("h6", container, "41 °F");
+  const forecastImage = createElementWithClass(
+    "img",
+    container,
+    "forecast-img"
+  );
+  forecastImage.src = "../dist/images/cloudy.svg";
+};
 export {
   createElement,
   createTextElement,
   createElementWithClass,
   createWeatherCell,
+  createForecastCell,
 };
