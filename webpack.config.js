d29cb599b7ed26440b9c9c67c42054a1c8383f0c
jpmunich <parkercoyle8@gmail.com>
2023-12-06 15:17:50 -0500

    Production Mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { Template } = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template } = require("webpack");
 
 module.exports = {
-mode: 'development',
-entry: {
-  index: './src/index.js',
-},
-  devtool: 'inline-source-map',
+  mode: "production",
+  entry: {
+    index: "./src/index.js",
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Odin Weather App',
-      template: './src/template.html',
+      title: "Odin Weather App",
+      template: "./src/template.html",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
